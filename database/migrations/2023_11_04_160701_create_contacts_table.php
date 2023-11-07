725dc47842d2017e907ae3303cb67149462b5e4d
zenenperaza <peraza@outlook.com>
2023-11-07 11:43:22 -0400

    first commit

--- database/migrations/2023_11_04_160701_create_contacts_table.php
+++ database/migrations/2023_11_04_160701_create_contacts_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('phone_number');
+            $table->string('email');
+            $table->tinyInteger('age', unsigned:true);
             $table->timestamps();
         });
     }
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -15,17 +15,54 @@
                             <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" required autocomplete="name" autofocus>
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" autocomplete="name" autofocus >
+                                @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                  <strong>{{ $message }}</strong>
+                                </span>
+                              @enderror
 
                             </div>
                         </div>
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">Phone</label>
+                            <label for="phone_number" class="col-md-4 col-form-label text-md-end">Phone</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="tel" class="form-control " name="phone_number"  required autocomplete="phone_number">
+                                <input id="phone_number" type="tel" class="form-control  @error('phone_number') is-invalid @enderror" name="phone_number"  value="{{ old('phone_number') }}"   autocomplete="phone_number">
+                                @error('phone_number')
+                                <span class="invalid-feedback" role="alert">
+                                  <strong>{{ $message }}</strong>
+                                </span>
+                              @enderror
+                          
+                            </div>
+                        </div>
 
+                        <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">Email</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="text" class="form-control  @error('email') is-invalid @enderror" name="email"  value="{{ old('email') }}"   autocomplete="email">
+                                @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                  <strong>{{ $message }}</strong>
+                                </span>
+                              @enderror
+                          
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="age" class="col-md-4 col-form-label text-md-end">Age</label>
+
+                            <div class="col-md-6">
+                                <input id="age" type="text" class="form-control  @error('age') is-invalid @enderror" name="age"  value="{{ old('age') }}"   autocomplete="age">
+                                @error('age')
+                                <span class="invalid-feedback" role="alert">
+                                  <strong>{{ $message }}</strong>
+                                </span>
+                              @enderror
                           
                             </div>
                         </div>
